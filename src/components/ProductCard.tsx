5a9baa0ac1b2e03176d34c60b4c4f0e55d9afd25
marc ibiza <marc@sklum.com>
2022-01-27 08:37:40 +0100

    Pasar css a module scss componentes

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,106 +1,112 @@
 // import { DraggableProvidedDraggableProps, DraggableProvidedDragHandleProps } from "react-beautiful-dnd";
+import styles from "./ProductCard.module.scss";
 import { useRef } from "react";
 import { ProductType } from "../type/Product";
-import { XYCoord } from 'dnd-core';
-import { useDrag, useDrop, DropTargetMonitor } from 'react-dnd';
+import { XYCoord } from "dnd-core";
+import { useDrag, useDrop, DropTargetMonitor } from "react-dnd";
 
 const style = {
-    border: '1px dashed gray',
-    padding: '0.5rem 1rem',
-    marginBottom: '.5rem',
-    backgroundColor: 'white',
-    cursor: 'move',
-  }
+  border: "1px dashed gray",
+  padding: "0.5rem 1rem",
+  marginBottom: ".5rem",
+  backgroundColor: "white",
+  cursor: "move",
+};
 
 export const ProductCard = ({ product, id, index, moveCard }: PropsType) => {
-    const ref = useRef(null);
+  const ref = useRef(null);
 
-    const [{ handlerId }, drop] = useDrop({
-        accept: 'PRODUCT_CARD',
-        collect(monitor) {
-            return {
-                handlerId: monitor.getHandlerId(),
-            }
-        },
-        hover(item: DragItem, monitor: DropTargetMonitor) {
-            if (!ref.current) {
-                return
-            }
-            const dragIndex = item.index
-            const hoverIndex = index
+  const [{ handlerId }, drop] = useDrop({
+    accept: "PRODUCT_CARD",
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item: DragItem, monitor: DropTargetMonitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
 
-            // Don't replace items with themselves
-            if (dragIndex === hoverIndex) {
-                return
-            }
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
 
-            // Determine rectangle on screen
-            const hoverBoundingRect = ref.current?.getBoundingClientRect()
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
 
-            // Get vertical middle
-            const hoverMiddleY =
-                (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
 
-            // Determine mouse position
-            const clientOffset = monitor.getClientOffset()
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
 
-            // Get pixels to the top
-            const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
+      // Get pixels to the top
+      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
 
-            // Only perform the move when the mouse has crossed half of the items height
-            // When dragging downwards, only move when the cursor is below 50%
-            // When dragging upwards, only move when the cursor is above 50%
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
 
-            // Dragging downwards
-            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-                return
-            }
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
 
-            // Dragging upwards
-            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-                return
-            }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
 
-            // Time to actually perform the action
-            moveCard(dragIndex, hoverIndex)
+      // Time to actually perform the action
+      moveCard(dragIndex, hoverIndex);
 
-            // Note: we're mutating the monitor item here!
-            // Generally it's better to avoid mutations,
-            // but it's good here for the sake of performance
-            // to avoid expensive index searches.
-            item.index = hoverIndex
-        },
-    })
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+  });
 
-    const [{ isDragging }, drag] = useDrag({
-        type: 'PRODUCT_CARD',
-        item: () => {
-            return { id, index }
-        },
-        collect: (monitor: any) => ({
-            isDragging: monitor.isDragging(),
-        }),
-    })
+  const [{ isDragging }, drag] = useDrag({
+    type: "PRODUCT_CARD",
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor: any) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
 
-    const opacity = isDragging ? 0 : 1
-    drag(drop(ref))
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
 
-    return (
-        <div ref={ref} style={{ ...style, opacity }} data-handler-id={handlerId}>
-            <h2>{product.name}</h2>
-        </div>
-    )
-}
+  return (
+    <div
+      ref={ref}
+      className={styles.gridItem}
+      style={{ ...style, opacity }}
+      data-handler-id={handlerId}
+    >
+      <h2>{product.name}</h2>
+    </div>
+  );
+};
 
 type PropsType = {
-    product: ProductType;
-    id?: string;
-    index?: number
-    moveCard?: (dragIndex: number, hoverIndex: number) => void;
-}
+  product: ProductType;
+  id?: string;
+  index?: number;
+  moveCard?: (dragIndex: number, hoverIndex: number) => void;
+};
 
 type DragItem = {
-    index: number
-    id: string
-    type: string
-}
+  index: number;
+  id: string;
+  type: string;
+};
--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -1,53 +1,55 @@
-import { useProductsContext } from "../context/products/Products.context"
+import styles from "./ProductsGrid.module.scss";
+import { useProductsContext } from "../context/products/Products.context";
 import { ProductCard } from "./ProductCard";
 // import { DragDropContext, Droppable, Draggable, DropResult, ResponderProvided } from 'react-beautiful-dnd';
 import { ProductType } from "../type/Product";
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { Card } from "../dnd/Card";
 
-
-
 export const ProductsGrid = (props: { children?: any }) => {
+  const { products, setProducts } = useProductsContext();
+  if (!products?.length) {
+    return <div>No hay productos</div>;
+  }
 
-    const { products, setProducts } = useProductsContext();
-    if (!products?.length) {
-        return <div>No hay productos</div>
-    }
+  const moveElement = (
+    productLIst: ProductType[],
+    startIndex: number,
+    endIndex: number
+  ) => {
+    const result = [...productLIst];
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
 
-    const moveElement = (productLIst: ProductType[], startIndex: number, endIndex: number) => {
-        const result = [...productLIst];
-        const [removed] = result.splice(startIndex, 1);
-        result.splice(endIndex, 0, removed);
-        return result;
-    }
+  // const handleProductsDragEnd = (result: DropResult, _provided: ResponderProvided) => {
+  //     if (!result?.destination) return;
+  //     if (result.source.index === result.destination.index) return;
+  //     setProducts(moveElement(products, result.source.index, result.destination.index));
+  // }
 
-    // const handleProductsDragEnd = (result: DropResult, _provided: ResponderProvided) => {
-    //     if (!result?.destination) return;
-    //     if (result.source.index === result.destination.index) return;
-    //     setProducts(moveElement(products, result.source.index, result.destination.index));
-    // }
+  const handleMoveCard = (dragIndex, hoverIndex) => {
+    const movedList = moveElement(products, dragIndex, hoverIndex);
+    setProducts(movedList);
+  };
 
-    const handleMoveCard = (dragIndex, hoverIndex) => {
-        const movedList = moveElement(products, dragIndex, hoverIndex);
-        setProducts(movedList);
-    }
-
-    return (
-        <div className="grid">
-            <DndProvider backend={HTML5Backend}>
-                {products.map((product, index) => {
-                    return (
-                        <ProductCard
-                            product={product}
-                            key={product.defaultCombinationId}
-                            index={index}
-                            moveCard={handleMoveCard}
-                            id={product.id.toString()}
-                        />
-                    )
-                })}
-            </DndProvider>
-        </div>
-    )
-}
+  return (
+    <div className={styles.grid}>
+      <DndProvider backend={HTML5Backend}>
+        {products.map((product, index) => {
+          return (
+            <ProductCard
+              product={product}
+              key={product.defaultCombinationId}
+              index={index}
+              moveCard={handleMoveCard}
+              id={product.id.toString()}
+            />
+          );
+        })}
+      </DndProvider>
+    </div>
+  );
+};
