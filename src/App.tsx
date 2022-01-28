e022e55368014b9dd27b953efa91cb1cf4b564aa
marc ibiza <marc@sklum.com>
2022-01-28 11:54:58 +0100

    Position sticky menu

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
               <SelectCategories></SelectCategories>
               <ButtonSendData></ButtonSendData>
             </div>
+
             <ProductsGrid></ProductsGrid>
           </div>
         </ProductsProvider>
