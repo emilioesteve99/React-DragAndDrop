0435bc7a4adf5ff6d21bf66b0f5dc663718489bf
marc ibiza <marc@sklum.com>
2022-01-28 12:20:10 +0100

    Detectar evento scroll para cambiar estilos del menu

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,37 @@ import { SelectCategories } from "./components/SelectCategories";
 import { ProductsGrid } from "./components/ProductsGrid";
 import { CurrentCategoryProvider } from "./context/categories/CurrentCategory.provider";
 import { ButtonSendData } from "./components/ButtonSendData";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [styleBorder, setStyleBorder] = useState({});
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    if (position > 200) {
+      setStyleBorder({
+        boxShadow: "rgb(17 17 27 / 15%) 0px 2px 3px",
+        width: "calc(100% + 60px",
+        marginLeft: "-30px",
+        marginRight: "-30px",
+      });
+    } else {
+      setStyleBorder({
+        boxShadow: "unset",
+        width: "unset",
+        marginLeft: "unset",
+        marginRight: "unset",
+      });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <CategoriesProvider>
       <CurrentCategoryProvider>
@@ -17,7 +46,7 @@ function App() {
               Arrastrar y soltar los productos en la posición que quieras
               actualizar de la PLP.
             </p>
-            <div className={styles.containerFilter}>
+            <div className={styles.containerFilter} style={styleBorder}>
               <SelectCategories></SelectCategories>
               <ButtonSendData></ButtonSendData>
             </div>
