4db409add445be266ce14e5ed22690dae403c987
in-emilio <emilio@sklum.com>
2022-01-28 13:41:32 +0100

    cambiando paginas a rutas

---
+++ src/pages/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./App.module.scss";
+import { CategoriesProvider } from "../context/categories/Categories.provider";
+import { ProductsProvider } from "../context/products/Products.provider";
+import { SelectCategories } from "../components/SelectCategories";
+import { ProductsGrid } from "../components/ProductsGrid";
+import { CurrentCategoryProvider } from "../context/categories/CurrentCategory.provider";
+import { ProductsTable } from "../components/ProductsTable";
+import { ButtonSendData } from "../components/ButtonSendData";
+
+export const App = () => {
+  return (
+    <CategoriesProvider>
+      <CurrentCategoryProvider>
+        <ProductsProvider>
+          <div className={styles.columnsContainer}>
+            <SelectCategories />
+            {/*<ProductsGrid />*/}
+            <ProductsTable />
+          </div>
+        </ProductsProvider>
+      </CurrentCategoryProvider>
+    </CategoriesProvider>
+    // <ProductsTable />
+  );
+}
+
+export default App;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,17 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+
 import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
-import App from "./pages/App";
 
 export const Home = () => {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/">
+                    <h1>{'hola'}</h1>
+                </Route>
             </Routes>
         </BrowserRouter>
     )
