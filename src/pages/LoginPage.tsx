6c8cb4d24362125c4c8cad29695904c10cfc35ec
marc ibiza <marc@sklum.com>
2022-01-31 09:49:29 +0100

    Ajuste de rutas login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,26 +3,24 @@ import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
-    const emailRef = useRef(null);
-    const passwordRef = useRef(null);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    const emailInputValue = emailRef.current.value;
+    const passwordInputValue = passwordRef.current.value;
+    console.log({ emailInputValue, passwordInputValue });
+    navigate("/grid");
+  };
 
-    const handleLoginSubmit = (event) => {
-        event.preventDefault();
-        const emailInputValue = emailRef.current.value;
-        const passwordInputValue = passwordRef.current.value;
-        console.log({emailInputValue, passwordInputValue});
-        navigate('/product-list/grid')
-    }
-
-    return (
-        <form onSubmit={handleLoginSubmit}>
-            <InputText ref={emailRef} type="email" placeholder="email"/>
-            <InputText ref={passwordRef} type="password" placeholder="password"/>
-            <input type="submit" value="Sign in" />
-        </form>
-    )
-
-}
+  return (
+    <form onSubmit={handleLoginSubmit}>
+      <InputText ref={emailRef} type="email" placeholder="email" />
+      <InputText ref={passwordRef} type="password" placeholder="password" />
+      <input type="submit" value="Sign in" />
+    </form>
+  );
+};
