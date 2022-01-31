858ba47364bfd5e10080a674c37ee33ee6a5f156
marc ibiza <marc@sklum.com>
2022-01-31 14:49:27 +0100

    Implementación Sweetalert2 cuando cambiamos de vista:
    Pedir confirmación con un modal.

--- src/components/NavigateButton.tsx
+++ src/components/NavigateButton.tsx
@@ -1,12 +1,30 @@
 import { Button } from "primereact/button";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export const NavigateButton = (props: PropsType) => {
   const navigate = useNavigate();
 
   const handleNavigateButton = (event) => {
     event.preventDefault();
-    navigate(props.path);
+
+    console.log(props.path);
+
+    Swal.fire({
+      title: "Si cambias de vista perderás la configuración, estas seguro?",
+      showDenyButton: true,
+      // showCancelButton: true,
+      confirmButtonText: "Sí",
+      denyButtonText: `No`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        navigate(props.path);
+      }
+      // else if (result.isDenied) {
+      //   Swal.fire("Puedes continuar", "", "info");
+      // }
+    });
   };
 
   return (
